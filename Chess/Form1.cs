2d7d93acaa06f7c3db98dbc2caaad46a45467fcc
HENRI SARÉN <henri.saren@edu.sakky.fi>
2022-10-05 18:12:16 +0300

    Changed from multiple classes to a singular one with functions

--- Chess/Form1.cs
+++ Chess/Form1.cs
@@ -17,46 +17,154 @@ namespace Chess
         public Button b;
         public int x;
         public int y;
-
+        public string Type;
+        public bool is_player;
         
-    }       
-    public class Pawn_Player : Piece
-    {
-        public bool is_pawn;
-        public void pawn_player_moves()
+        // Sets the arrays for legal moves
+        public void move()
+        {
+            if (b.Text != "")
             {
-            bool[,] pawn_player_moves = new bool[5, 5];
-            pawn_player_moves[2, 3] = true;
-            pawn_player_moves[2, 4] = true;
-            pawn_player_moves[3, 3] = true;
-            pawn_player_moves[1, 3] = true;
+                Type = b.Text;
             }
-        
+            if (Type == "P")
+            {
+                bool[,] pawn_moves = new bool[5, 5];
+                pawn_moves[2, 3] = true;
+                pawn_moves[2, 4] = true;
+                pawn_moves[3, 3] = true;
+                pawn_moves[1, 3] = true;
+                
+            }
+            if (Type == "R")
+            {
+                bool[,] rook_player_moves = new bool[7, 7];
+                rook_player_moves[2, 3] = true;
+                rook_player_moves[1, 3] = true;
+                rook_player_moves[0, 3] = true;
+                rook_player_moves[4, 3] = true;
+                rook_player_moves[5, 3] = true;
+                rook_player_moves[6, 3] = true;
+                rook_player_moves[3, 4] = true;
+                rook_player_moves[3, 5] = true;
+                rook_player_moves[3, 6] = true;
+                rook_player_moves[3, 2] = true;
+                rook_player_moves[3, 1] = true;
+                rook_player_moves[3, 0] = true;
+            }
+            if (Type == "K")
+            {
+                bool[,] king_moves = new bool[3, 3];
+                king_moves[0, 0] = true;
+                king_moves[1, 0] = true;
+                king_moves[2, 0] = true;
+                king_moves[0, 1] = true;
+                king_moves[2, 1] = true;
+                king_moves[0, 2] = true;
+                king_moves[1, 2] = true;
+                king_moves[2, 2] = true;
+            }
+            if (Type == "B")
+            {
+                bool[,] bishop_moves = new bool[7, 7];
+                bishop_moves[2, 2] = true;
+                bishop_moves[1, 1] = true;
+                bishop_moves[0, 0] = true;
+                bishop_moves[4, 4] = true;
+                bishop_moves[5, 5] = true;
+                bishop_moves[6, 6] = true;
+                bishop_moves[0, 6] = true;
+                bishop_moves[1, 5] = true;
+                bishop_moves[2, 4] = true;
+                bishop_moves[4, 2] = true;
+                bishop_moves[5, 1] = true;
+                bishop_moves[6, 0] = true;
+            }
+            if (Type == "Kn")
+            {
+                bool[,] knight_moves = new bool[5, 5];
+                knight_moves[1, 0] = true;
+                knight_moves[3, 0] = true;
+                knight_moves[0, 1] = true;
+                knight_moves[4, 1] = true;
+                knight_moves[4, 3] = true;
+                knight_moves[0, 3] = true;
+                knight_moves[1, 4] = true;
+                knight_moves[3, 4] = true;
+            }
+            if (Type == "Q")
+            {
+                bool[,] queen_moves = new bool[7, 7];
+                queen_moves[0, 0] = true;
+                queen_moves[1, 1] = true;
+                queen_moves[2, 2] = true;
+                queen_moves[4, 4] = true;
+                queen_moves[5, 5] = true;
+                queen_moves[6, 6] = true;
+                queen_moves[0, 3] = true;
+                queen_moves[1, 3] = true;
+                queen_moves[2, 3] = true;
+                queen_moves[4, 3] = true;
+                queen_moves[5, 3] = true;
+                queen_moves[6, 3] = true;
+                queen_moves[3, 0] = true;
+                queen_moves[3, 1] = true;
+                queen_moves[3, 2] = true;
+                queen_moves[3, 4] = true;
+                queen_moves[3, 5] = true;
+                queen_moves[3, 6] = true;
+                queen_moves[0, 6] = true;
+                queen_moves[1, 5] = true;
+                queen_moves[2, 4] = true;
+                queen_moves[4, 2] = true;
+                queen_moves[5, 1] = true;
+                queen_moves[6, 0] = true;
+            }
+        }
 
-    }
-    public class Rook_Player : Piece
-    {
+        // Sets the color and texts of the buttons
+        public void color_text()
+        {
+            if (is_player == true)
+            {
+                b.ForeColor = Color.Gray;
+            }
+            if (is_player != true)
+            {
+                b.ForeColor = Color.Black;
+            }
+            if (Type == "P")
+            {
+                b.Text = "P";
+            }
+            if (Type == "Q")
+            {
+                b.Text = "Q";
+            }
+            if (Type == "K")
+            {
+                b.Text = "K";
+            }
+            if (Type == "R")
+            {
+                b.Text = "R";
+            }
+            if (Type == "Kn")
+            {
+                b.Text = "Kn";
+            }
+            if (Type == "B")
+            {
+                b.Text = "B";
+            }
+        }
 
-    }
-    public class Bishop_Player : Piece
-    {
-
-    }
-    public class Knight : Piece
-    {
-
-    }
-    public class Queen : Piece
-    {
-
-    }
-    public class King : Piece
-    {
-
-    }
+    }       
+ 
     public partial class Form1 : Form
     {
-        bool player_turn;
+        bool player_turn = true;
+        int turn_num = 0;
         string[,] board = new string[8, 8];
         Piece[,] b_board = new Piece[8, 8];
         string[,] b_clicked = new string[8, 8];
@@ -94,22 +202,15 @@ namespace Chess
                         b.Text = "Q";
                     if (b.Name == "4_0" || b.Name == "4_7")
                         b.Text = "K";
+
                     // Colors the white piece names to gray
                     if (b.Name == $"{i}_6" || b.Name == $"{i}_7")
                         b.ForeColor = Color.Gray;
+
                     // Disables all other buttons exept those containing a white piece
                     if (b.Name == $"{i}_0" || b.Name == $"{i}_1")
                         b.Enabled = false;
-                    // This is the old way I did this, left it here to remind me to never fucking do it again.
-                    /*if (b.Name == "0_0" || b.Name == "2_0" || b.Name == "4_0" || b.Name == "6_0" ||
-                        b.Name == "1_1" || b.Name == "3_1" || b.Name == "5_1" || b.Name == "7_1" ||
-                        b.Name == "0_2" || b.Name == "2_2" || b.Name == "4_2" || b.Name == "6_2" ||
-                        b.Name == "1_3" || b.Name == "3_3" || b.Name == "5_3" || b.Name == "7_3" ||
-                        b.Name == "0_4" || b.Name == "2_4" || b.Name == "4_4" || b.Name == "6_4" ||
-                        b.Name == "1_5" || b.Name == "3_5" || b.Name == "5_5" || b.Name == "7_5" ||
-                        b.Name == "0_6" || b.Name == "2_6" || b.Name == "4_6" || b.Name == "6_6" ||
-                        b.Name == "1_7" || b.Name == "3_7" || b.Name == "5_7" || b.Name == "7_7")
-                        b.BackColor = Color.SandyBrown;*/
+
                     // Colors the board correctly
                     if ((i % 2 == 0 && j % 2 == 0) || (i == 0 && j == 0))
                         b.BackColor = Color.White;
@@ -129,15 +230,8 @@ namespace Chess
         private void newButton_CLick(object sender, EventArgs e)
         {
             Button b = (Button)sender;
+
             
-            foreach (Control c in this.Controls)
-            {
-                string[] b_name = b.Name.Split('_');
-                int x = Convert.ToInt32(b_name[0]);
-                int y = Convert.ToInt32(b_name[1]);
-                b_clicked[x, y] = "Clicked";
-                
-            }
         }
     }
 }
