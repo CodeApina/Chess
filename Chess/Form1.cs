d954a0b2c72b08a645aab736ac94193d2983dc1a
Henri Sarén <henri.saren@gmail.com>
2022-10-22 22:40:49 +0300

    Started reworking piece system

--- Chess/Form1.cs
+++ Chess/Form1.cs
@@ -14,161 +14,65 @@ namespace Chess
 
     public class Piece
     {
-        public Button b;
         public int x;
         public int y;
-        public string Type;
-        public bool is_player;
-        
-        public void Player()
+        public string piece_text;
+        public bool is_on_board = true;
+
+    }
+    public sealed class Pawn : Piece
+    {
+        public Pawn(int x, int y, string piece_text)
         {
-            is_player = true;
+            piece_text = piece_text;
+            x = x;
+            y = y;
         }
 
-
-        // Sets the arrays for legal moves
-        public void Move_set()
+    }
+    public sealed class Rook : Piece
+    {
+        public Rook(int x, int y, string piece_text)
         {
-            if (b.Text != string.Empty)
-            {
-                Type = b.Text;
-            }
-            if (Type == "P")
-            {
-                bool[,] pawn_moves = new bool[5, 5];
-                pawn_moves[2, 3] = true;
-                pawn_moves[2, 4] = true;
-                pawn_moves[3, 3] = true;
-                pawn_moves[1, 3] = true;
-                
-            }
-            if (Type == "R")
-            {
-                bool[,] rook_player_moves = new bool[7, 7];
-                rook_player_moves[2, 3] = true;
-                rook_player_moves[1, 3] = true;
-                rook_player_moves[0, 3] = true;
-                rook_player_moves[4, 3] = true;
-                rook_player_moves[5, 3] = true;
-                rook_player_moves[6, 3] = true;
-                rook_player_moves[3, 4] = true;
-                rook_player_moves[3, 5] = true;
-                rook_player_moves[3, 6] = true;
-                rook_player_moves[3, 2] = true;
-                rook_player_moves[3, 1] = true;
-                rook_player_moves[3, 0] = true;
-            }
-            if (Type == "K")
-            {
-                bool[,] king_moves = new bool[5, 3];
-                king_moves[1, 0] = true;
-                king_moves[2, 0] = true;
-                king_moves[3, 0] = true;
-                king_moves[1, 1] = true;
-                king_moves[3, 1] = true;
-                king_moves[1, 2] = true;
-                king_moves[2, 2] = true;
-                king_moves[3, 2] = true;
-                king_moves[4, 1] = true;
-                king_moves[0, 1] = true;
-            }
-            if (Type == "B")
-            {
-                bool[,] bishop_moves = new bool[7, 7];
-                bishop_moves[2, 2] = true;
-                bishop_moves[1, 1] = true;
-                bishop_moves[0, 0] = true;
-                bishop_moves[4, 4] = true;
-                bishop_moves[5, 5] = true;
-                bishop_moves[6, 6] = true;
-                bishop_moves[0, 6] = true;
-                bishop_moves[1, 5] = true;
-                bishop_moves[2, 4] = true;
-                bishop_moves[4, 2] = true;
-                bishop_moves[5, 1] = true;
-                bishop_moves[6, 0] = true;
-            }
-            if (Type == "Kn")
-            {
-                bool[,] knight_moves = new bool[5, 5];
-                knight_moves[1, 0] = true;
-                knight_moves[3, 0] = true;
-                knight_moves[0, 1] = true;
-                knight_moves[4, 1] = true;
-                knight_moves[4, 3] = true;
-                knight_moves[0, 3] = true;
-                knight_moves[1, 4] = true;
-                knight_moves[3, 4] = true;
-            }
-            if (Type == "Q")
-            {
-                bool[,] queen_moves = new bool[7, 7];
-                queen_moves[0, 0] = true;
-                queen_moves[1, 1] = true;
-                queen_moves[2, 2] = true;
-                queen_moves[4, 4] = true;
-                queen_moves[5, 5] = true;
-                queen_moves[6, 6] = true;
-                queen_moves[0, 3] = true;
-                queen_moves[1, 3] = true;
-                queen_moves[2, 3] = true;
-                queen_moves[4, 3] = true;
-                queen_moves[5, 3] = true;
-                queen_moves[6, 3] = true;
-                queen_moves[3, 0] = true;
-                queen_moves[3, 1] = true;
-                queen_moves[3, 2] = true;
-                queen_moves[3, 4] = true;
-                queen_moves[3, 5] = true;
-                queen_moves[3, 6] = true;
-                queen_moves[0, 6] = true;
-                queen_moves[1, 5] = true;
-                queen_moves[2, 4] = true;
-                queen_moves[4, 2] = true;
-                queen_moves[5, 1] = true;
-                queen_moves[6, 0] = true;
-            }
+            piece_text = piece_text;
+            x = x;
+            y = y;
         }
-
-        // Sets the color and texts of the buttons
-        public void Color_text()
+    }
+    public sealed class Knight : Piece
+    {
+        public Knight(int x, int y, string piece_text)
         {
-            if (is_player == true)
-            {
-                b.ForeColor = Color.Gray;
-            }
-            if (is_player != true)
-            {
-                b.ForeColor = Color.Black;
-            }
-            if (Type == "P")
-            {
-                b.Text = "P";
-            }
-            if (Type == "Q")
-            {
-                b.Text = "Q";
-            }
-            if (Type == "K")
-            {
-                b.Text = "K";
-            }
-            if (Type == "R")
-            {
-                b.Text = "R";
-            }
-            if (Type == "Kn")
-            {
-                b.Text = "Kn";
-            }
-            if (Type == "B")
-            {
-                b.Text = "B";
-            }
+            piece_text = piece_text;
+            x = x;
+            y = y;
         }
-        public void Move()
+    }
+    public sealed class Bishop : Piece
+    {
+        public Bishop(int x, int y, string piece_text)
         {
-            
+            piece_text = piece_text;
+            x = x;
+            y = y;
+        }
+    }
+    public sealed class Queen : Piece
+    {
+        public Queen(int x, int y, string piece_text)
+        {
+            piece_text = piece_text;
+            x = x;
+            y = y;
+        }
+    }
+    public sealed class King : Piece 
+    {
+        public King(int x, int y, string piece_text)
+        {
+            piece_text = piece_text;
+            x = x;
+            y = y;
         }
     }
 
@@ -176,30 +80,65 @@ namespace Chess
     {
         string[,] board = new string[8, 8];
         public Button[,] b_board = new Button[8, 8];
+        public Piece m_selected_piece = null;
+        public Piece[] player_pieces = new Piece[8];
+        public Piece[] ai_pieces = new Piece[8];
+        
+        public void Initialize_Pieces()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                player_pieces[i] = new Pawn(i, 6, "P");
+                ai_pieces[i] = new Pawn(i,1,"P");
+            }
+
+
+        }
 
         public Form1()
         {
             InitializeComponent();
-            Button_create();
+            Initialize_Pieces();
+            Button_create(m_selected_piece);
+            
+        }
+
+        public void Place_Pieces(int x, int y, Piece piece)
+        {;
+            //for (int i = 0; i < 16; i++)
+            //{
+            //    x = player_pieces[i].x;
+            //    y = player_pieces[i].y;
+            //    if (b.Name == $"{x}_{y}")
+            //    {
+            //        b.Text = player_pieces[i].piece_text;
+            //    }
+            //    ai_pieces[(int)i].x = x;
+            //    ai_pieces[(int)i].y = y;
+            //    if(b.Name == $"{x}_{y}")
+            //    {
+            //        b.Text = ai_pieces[i].piece_text;
+            //    }
+            //}
         }
 
     
-        public void Button_create()
+        public void Button_create(Piece piece)
         {
-            for (int j = 0; j < 8; j++)
-                for (int i = 0; i < 8; i++)
+            for (int y = 0; y < 8; y++)
+                for (int x = 0; x < 8; x++)
                 {
 
                     Button b = new Button();
                     b.Size = new Size(50, 50);
-                    b.Location = new Point(100 + 50 * i, 100 + 50 * j);
+                    b.Location = new Point(100 + 50 * x, 100 + 50 * y);
                     // Sets up the button names
-                    b.Name = $"{i}_{j}";
-                    if (b.Name == $"{i}_6")
+                    b.Name = $"{x}_{y}";
+                    /*if (b.Name == $"{x}_6")
                     {
                         b.Text = "P";
                     }
-                    if (b.Name == $"{i}_1")
+                    if (b.Name == $"{x}_1")
                         b.Text = "P";
                     if (b.Name == "0_0" || b.Name == "0_7" || b.Name == "7_7" || b.Name == "7_0")
                         b.Text = "R";
@@ -211,50 +150,79 @@ namespace Chess
                         b.Text = "Q";
                     if (b.Name == "4_0" || b.Name == "4_7")
                         b.Text = "K";
+                    foreach (Piece piece1 in player_pieces)
+                    {
+
+                    }
+                    for (int i = 0; i < 8; i++)
+                    {
+                        x = player_pieces[(int)i].x;
+                        y = player_pieces[(int)i].y;
+                        if (b.Name == $"{x}_{y}")
+                        {
+                            b.Text = player_pieces[i].piece_text;
+                        }
+                        ai_pieces[(int)i].x = x;
+                        ai_pieces[(int)i].y = y;
+                        if (b.Name == $"{x}_{y}")
+                        {
+                            b.Text = ai_pieces[i].piece_text;
+                        }
+                    }
 
                     // Colors the white piece names to gray
-                    if (b.Name == $"{i}_6" || b.Name == $"{i}_7")
-                        b.ForeColor = Color.Gray;
+                    if (b.Name == $"{x}_6" || b.Name == $"{x}_7")
+                        b.ForeColor = Color.Gray;*/
 
-                    // Disables all other buttons exept those containing a white piece
-                    if (b.Name == $"{i}_0" || b.Name == $"{i}_1")
-                        b.Enabled = false;
-
+                    b.Enabled = false;
                     // Colors the board correctly
-                    if ((i % 2 == 0 && j % 2 == 0) || (i == 0 && j == 0))
+                    if ((x % 2 == 0 && y % 2 == 0) || (x == 0 && y == 0))
                         b.BackColor = Color.White;
-                    else if (i % 2 != 0 && j % 2 != 0)
+                    else if (x % 2 != 0 && y % 2 != 0)
                         b.BackColor = Color.White;
                     else b.BackColor = Color.SandyBrown;
-
-                    if (b.Text == string.Empty)
-                        b.Enabled = false;
                     this.Controls.Add(b);
                     b.Click += new EventHandler(NewButton_CLick);
-                    b_board[i, j] = b;
+                    b_board[x, y] = b;
                 }
         }
-         
+        
+        public void Process_Move(Piece selected_piece, int x, int y)
+        {
+
+        }
 
         private void NewButton_CLick(object sender, EventArgs e)
         {
-            Button b = (Button)sender;
-
-            var p = new Piece();
-            // Set's is_player as true
-            p.Player();
-            // Sets Piece.b as sender button
-            p.b = b;
-            // Runs Move_set
-            p.Move_set();
-            // Set's button forecolor
-            p.Color_text();
+            Button button = (Button)sender;
             // Get's buttons x and y co-ordinates
-            string[] buttonName = b.Name.Split('_');
+            string[] buttonName = button.Name.Split('_');
             int x = Convert.ToInt32(buttonName[0]);
             int y = Convert.ToInt32(buttonName[1]);
-            p.x = x;
-            p.y = y;
+
+            //deselect selected piece
+            if (m_selected_piece != null && m_selected_piece.x == x && m_selected_piece.y == y)
+                m_selected_piece = null;
+
+            //updates selection if needed
+            foreach (Piece piece in player_pieces)
+            {
+                if (piece.x == x && piece.y == y)
+                {
+                    m_selected_piece = piece;
+                }
+            }
+            foreach (Piece piece in ai_pieces)
+            {
+                if (piece.x == x && piece.y == y)
+                {
+                    m_selected_piece = piece;
+                }
+            }
+            if (m_selected_piece != null && m_selected_piece.x != x && m_selected_piece.y != y)
+            {
+                Process_Move(m_selected_piece, x, y);
+            }
             
         }
     }
